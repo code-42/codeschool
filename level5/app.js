92eab1f2d1f64a0acc58cfed99d834de168dd0c9
Edward Dupre <edwd42@gmail.com>
2017-08-27 18:06:51 +0000

    complete challenge 5.3 Route Params

--- level5/app.js
+++ level5/app.js
@@ -1,69 +1,29 @@
-// 5.2 Express Routes 250 pts
+// 5.3 Route Params 230 pts
 
-// Let's create an express route that accepts GET requests on '/tweets' 
-// and responds by sending back a static HTML file.
+// Let's create a route that accepts dynamic arguments in the URL path 
+// and responds with the quote from the proper author.
 
-// First, Create a GET route for '/tweets' and give it the proper callback. The callback function should accept two arguments: the request and the response.
-// Second task, Send back the file tweets.html, which lives under the project's root path. Remember to use __dirname to locate tweets.html.
-// Third task, Finally, have the express app listen on port 8080.
+// Task 1/2 Start by creating a GET route for '/quotes' that takes a
+// name parameter as part of the URL path.
+
+// Task 2/2 Now, use the name parameter from the URL to retrieve a quote 
+// from the quotes object and write it out to the response. Note: No piping here, 
+// just write the quote string to the response like you did in previous levels 
+// (and then close the response).
 
 var express = require('express');
 var app = express();
 
-app.get('/tweets', function(request, response){
-  response.sendFile(__dirname + '/tweets.html');
+var quotes = {
+  'einstein': 'Life is like riding a bicycle. To keep your balance you must keep moving',
+  'berners-lee': 'The Web does not just connect machines, it connects people',
+  'crockford': 'The good thing about reinventing the wheel is that you can get a round one',
+  'hofstadter': 'Which statement seems more true: (1) I have a brain. (2) I am a brain.'
+};
+
+app.get('/quotes/:name', function(request, response){
+  response.end(quotes[request.params.name]);
 });
+
 app.listen(8080);
 
-
-/*
-var request = require('request');
-var url = require('url');
-var app = require('http');
-
-// route definition
-// app.get('/tweets/:username', function(req, res){
-//     var username = req.params.username;
-//     options = {
-//         protocol: 'http',
-//         host: 'api.twitter.com',
-//         pathname: '/1/statuses/user_timeline.json',
-//         // get the last 10 tweets for screen_name
-//         query: { screen_name: username, count:10 }
-//     }
-//     var twitterUrl = url.format(options);
-    
-//     // pipe the request to response
-//     request(twitterUrl).pipe(response);
-// });
-
-// npm install prettyjson -g
-// npm install --save ejs
-
-// run node app.js in one terminal
-// open another terminal and run
-// curl -s http://localhost:8080/tweets/<screen_name> | prettyjson
-
-var express = require('express');
-var app = express();
-
-app.get('/tweets/:username', function(req, res){
-    var username = req.params.username;
-    options = {
-        protocol: 'http',
-        host: 'api.twitter.com',
-        pathname: '/1/statuses/user_timeline.json',
-        // get the last 10 tweets for screen_name
-        query: { screen_name: username, count:10 }
-    }
-    var twitterUrl = url.format(options);
-    
-    // pipe the request to response
-    // request(twitterUrl).pipe(response);
-    request(url, function(err, res, body){
-        var tweets = JSON.parse(body);
-        response.locals = {tweets: tweets, name: username};
-        response.render('tweets.ejs');
-    });
-});
-*/
