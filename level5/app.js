7a483992e259c2da067c7a0f8a7735569f3cd94a
Edward Dupre <edwd42@gmail.com>
2017-08-27 23:50:18 +0000

    complete challenge 5.6 Doing the Request

--- level5/app.js
+++ level5/app.js
@@ -1,22 +1,25 @@
-// 5.5 URL Building 240 pts
+// 5.6 Doing the Request 240 pts
 
-// Let's create a page which calls the Twitter search API and displays 
-// the last few results for Code School. The first step is to construct 
-// the proper URL, which is all you need to do in this challenge.
-
-// Complete the URL options which will be sent into the the url module's format method. 
-// The URL you'll want to construct is the following: 
-// http://search.twitter.com/search.json?q=codeschool
+// Next, we'll use the request module to make a simple web request 
+//and log the response to the console. You can use this example in the README.
+// To start, require the request module and assign the return function to a variable.
+// Next, issue a request to searchURL. Remember, the callback function for the 
+// request function takes three arguments: error, response and body.
+// Finally, log the response body to the console using console.log().
 
 var url = require('url');
 
 var options = {
-  // add URL options here
-  protocol: 'http',
-  host: 'search.twitter.com',
+  protocol: "http:",
+  host: "search.twitter.com",
   pathname: '/search.json',
-  query: {q: 'codeschool'}
+  query: { q: "codeschool"}
 };
 
 var searchURL = url.format(options);
-console.log(searchURL);
+
+var request = require('request');
+
+request(searchURL, function(error, response, body){
+  console.log(body);
+});
