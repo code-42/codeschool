25dd764e76d338f77be79827cd4f4f3948659ed8
Edward Dupre <edwd42@gmail.com>
2017-08-28 01:40:38 +0000

    complete challenge 6.5 Broadcasting Questions

--- level6/app.js
+++ level6/app.js
@@ -1,14 +1,23 @@
-// 6.2 Setting Up socket.io Server-Side 250 pts
+// 6.5 Broadcasting Questions 240 pts
 
-// So far we've created an Express server. Now we want to start building 
-// a real-time Q&A moderation service and we've decided to use socket.io.
+// When a question is submitted to our server, we want to broadcast it out 
+// to all the connected clients so they can have a chance to answer it.
+
+// First, In the server, listen for 'question' events from clients.
+// Then, emit the 'question' event on all the other clients connected, 
+// passing them the question data.
 
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-io.on('connection', function(client){
-  console.log('client connnected...');
+io.on('connection', function(client) {
+  console.log("Client connected...");
+  client.on('question', function(data) {
+    io.emit('question', data);
 });
+  
+});
+
 server.listen(8080);
