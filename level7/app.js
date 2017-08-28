5f83b6b3888820f3883ba6958954db585632b685
Edward Dupre <edwd42@gmail.com>
2017-08-28 15:02:48 +0000

    complete challenge 7.4 Working With Lists

--- level7/app.js
+++ level7/app.js
@@ -24,3 +24,31 @@ var client = redis.createClient();
 client.get('question', function(error, data){
   console.log(data);
 });
+
+// 7.4 Working With Lists 1 250 pts
+
+// As we saw in the video, redis can do more than just simple key-value pairs. 
+// We are going to be using redis' LISTS later to add persistence to our live-moderation app, 
+// so let's practice using them now.
+
+// Task 1/2 Using the redis client's lpush command, insert question1 into the questions list. 
+// Then, console.log the result you receive. Remember, the lpush function takes a callback 
+// as its last argument, which expects an error and value to be passed as arguments.
+// Task 2/2 Using the redis client's lpush command, insert question2 into the questions list. 
+// Then console.log the result you receive.
+
+var redis = require('redis');
+var client = redis.createClient();
+
+var question1 = "Where is the dog?";
+var question2 = "Where is the cat?";
+
+client.lpush('questions', question1, function(error, value){
+  console.log(value);
+});
+
+client.lpush('questions', question2, function(error, value){
+  console.log(value);
+});
+
+
