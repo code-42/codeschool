928ecbab05e9eea0aa1969c54344742de22d5ca4
Edward Dupre <edwd42@gmail.com>
2017-08-28 15:29:56 +0000

    complete challenge 7.8 Limiting Questions Stored

--- level7/app.js
+++ level7/app.js
@@ -134,3 +134,41 @@ io.sockets.on('connection', function(client) {
   });
 });
 
+// 7.8 Limiting Questions Stored 250 pts
+
+// Great work! One last thing though, since every time a new question comes in 
+// we store it in the questions list, we might run into a problem where there 
+// are just too many questions stored in that list.
+// Task 1/2 Add a callback to lpush that will be used to limit the size of the list down to a max of 20.
+// Task 2/2 Use the ltrim command to limit the size of the list stored within redis to a maximum size of 20. 
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+io.sockets.on('connection', function(client) {
+  redisClient.lrange("questions", 0, -1, function(err, questions) {
+    questions.forEach(function(question) {
+      client.emit("question", question);
+    });
+  });
+
+  client.on('answer', function(question, answer) {
+    client.broadcast.emit('answer', question, answer);
+  });
+
+  client.on('question', function(question) {
+    if(!client.question_asked) {
+      client.question_asked = true;
+      client.broadcast.emit('question', question);
+      redisClient.lpush("questions", question, function(err, response){
+        redisClient.ltrim('questions', 0, 19);
+      });
+    }
+  }); 
+
+});
