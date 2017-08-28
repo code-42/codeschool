9f4e734e8509572aa7e48fa1d62726f0352ae13d
Edward Dupre <edwd42@gmail.com>
2017-08-28 15:11:11 +0000

    complete challenge 7.6 Persisting Questions

--- level7/app.js
+++ level7/app.js
@@ -65,3 +65,34 @@ var client = redis.createClient();
 client.lrange('questions', 0, -1, function(err, questions){
   console.log(questions);
 });
+
+
+// 7.6 Persisting Questions 240 pts
+
+// Let's go back to our live-moderation app and add some persistence, 
+// first to the questions people ask.
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var socket = require('socket.io');
+var io = socket.listen(server);
+
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+io.sockets.on('connection', function(client) {
+  client.on('answer', function(question, answer) {
+    client.broadcast.emit('answer', question, answer);
+  });
+
+  client.on('question', function(question) {
+    if(!client.question_asked) {
+      client.question_asked = true;
+      client.broadcast.emit('question', question);
+      // add the question to the list here
+      redisClient.lpush('questions', question);
+    }
+  });
+});
+ 
